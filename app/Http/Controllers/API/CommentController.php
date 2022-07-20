6b29daad5c4c382a0e548dd6862e6e83a44cc1c5
pbijampola <patrickbijampola@gmail.com>
2022-07-20 11:28:00 +0300

    initial commit

---
+++ app/Http/Controllers/API/CommentController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments=Comment::all();
+        return response()->json($comments);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            "name"=>'required|string|max:15',
+            "comment"=>"required|string|max:200"
+        ]);
+
+        $newcomment=Comment::create([
+            'name'=>$request->name,
+            'comment'=>$request->comment
+
+        ]);
+        return response()->json($newcomment);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $comment = Comment::findOrFail($id);
+        return response()->json($comment);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validated=request()->validate([
+            "name"=>'required|string|max:15',
+            "comment"=>"required|string|max:200"
+        ]);
+        $comment=Comment::find($id);
+        $comment->update($validated);
+        return response()->json($comment);
+
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if($id=!null){
+            $comment=Comment::where('id',$id);
+            $comment->delete();
+            return response()->json($comment::all());
+        }
+    }
+}
